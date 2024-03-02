e53a1064888678e449f1c1672cf144b097eab768
IkeEitatsu <yongdachi806@gmail.com>
2024-03-03 01:36:16 +0900

    Update DataPlot_Optimization.py

--- DataPlot_Optimization.py
+++ DataPlot_Optimization.py
@@ -1,5 +1,8 @@
+import matplotlib
 import pandas as pd
+matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
+matplotlib.rc('font', **{'family':'Yu Gothic'})
 
 # エクセルファイルからデータを読み込む
 excel_file = "C:\\Users\\eitat\\OneDrive\\ドキュメント\\取り込み用_新電力密度.xlsx"  # ファイル名を適切なものに変更する
@@ -12,13 +15,13 @@ battery_B = df[(df['電圧'] >= 0) & (df['電圧'] <= 0.65)].reset_index(drop=Tr
 battery_C = df[(df['電圧'] >= 0) & (df['電圧'] <= 0.65)].reset_index(drop=True)
 
 # グラフを描画する
-plt.plot(battery_A['電圧'], battery_A['電力密度'], label='Initial')
-plt.plot(battery_B['電圧'], battery_B['電力密度'], label='After Topology Optimization')
-plt.plot(battery_C['電圧'], battery_C['電力密度'], label='After Shape Optimization')
+plt.plot(battery_A['電圧'], battery_A['電力密度'], label='最適化前', color = "red")
+plt.plot(battery_B['電圧'], battery_B['電力密度'], label='トポロジー最適化後', color = "blue")
+plt.plot(battery_C['電圧'], battery_C['電力密度'], label='形状最適化後', color = "black")
 
 # グラフのタイトルとラベルを設定
-plt.xlabel("Applied Voltage [V]")
-plt.ylabel("Maximum Power Density [uW/cm2]")
+plt.xlabel("印可電圧 [V]")
+plt.ylabel("電力密度 [uW/cm2]")
 
 # 凡例を表示
 plt.legend()
